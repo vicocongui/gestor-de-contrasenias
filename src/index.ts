19db412dcb4f83f45599e72d186ea6b1ba91660c
Martina <mfranco0305@gmail.com>
2024-06-27 17:41:40 -0300

    cors correct

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { mostrarListadoCuentas, test, agregarCuenta, actualizarCuenta, generarContraseniaSegura, consultarListado } from "./Modelo";
-import cors from "cors";
 dotenv.config();
+
+import * as cors from 'cors';
+const corsMiddleware: typeof cors = require('cors');
+
 
 const port = process.env.PORT || 3000;
 
@@ -10,7 +13,7 @@ const app: Express = express();
 
 // Middleware para parsear el cuerpo de las solicitudes en formato JSON
 app.use(express.json());
-app.use(cors());
+
 
 app.get("/v1/test", async (req: Request, res:Response)=>{
     try{
